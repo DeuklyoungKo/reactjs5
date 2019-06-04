55c1ffc65b13cc2de2169af35d04d718359ec18b
DeuklyoungKo <deutschgon@gmail.com>
2019-06-04 10:08:32 +0200

    chapter 3 (https://symfonycasts.com/screencast/reactjs)

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+/*
 
 const el = React.createElement(
     'h2',
@@ -7,6 +8,10 @@ const el = React.createElement(
     'Lift history!',
     React.createElement('span', null, 'hart')
 );
+*/
+
+const el = <h2>Lift Stuff! <span>hart</span></h2>
+
+console.log(el);
 
 ReactDom.render(el, document.getElementById('lift-stuff-app'));
-console.log(el);
--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,8 @@ Encore
 
     .disableSingleRuntimeChunk()
 
+    .enableReactPreset()
+
 ;
 
 

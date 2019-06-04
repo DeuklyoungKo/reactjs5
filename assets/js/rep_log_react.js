d2fa61549b82f8d046371600bda7d0dcba3ff114
DeuklyoungKo <deutschgon@gmail.com>
2019-06-04 09:39:56 +0200

    chapter 2 (https://symfonycasts.com/screencast/reactjs)

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,1 +1,12 @@
-console.log('Oh hallo React peeps!');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const el = React.createElement(
+    'h2',
+    null,
+    'Lift history!',
+    React.createElement('span', null, 'hart')
+);
+
+ReactDom.render(el, document.getElementById('lift-stuff-app'));
+console.log(el);

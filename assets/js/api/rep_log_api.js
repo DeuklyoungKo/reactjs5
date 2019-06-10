e6767eb7a57f27c3ac07b79ba10d325759560fe1
DeuklyoungKo <deutschgon@gmail.com>
2019-06-10 17:22:40 +0200

    chapter 30 (https://symfonycasts.com/screencast/reactjs)

--- assets/js/api/rep_log_api.js
+++ assets/js/api/rep_log_api.js
@@ -1,13 +1,25 @@
+function fetchJson(url, options) {
+    return fetch(url, Object.assign({
+        credentials: 'same-origin'
+    }, options))
+        .then(response => {
+            return response.json();
+        });
+}
+
+
 /**
  * Returns a Promise object with the rep logs data
  *
  * @returns {Promise<any | never>}
  */
 export function getRepLogs() {
-    return fetch('/reps',{
-        credentials: 'same-origin'
-    })
-        .then(response => {
-            return response.json().then((data) => data.items);
-        });
+    return fetchJson('/reps')
+        .then(data => data.items);
+}
+
+export function deleteRepLog(id) {
+    return fetchJson(`/reps/${id}`, {
+        method: 'DELETE'
+    });
 }

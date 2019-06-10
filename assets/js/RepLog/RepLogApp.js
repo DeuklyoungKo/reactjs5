4908659999fa81b3924d584df0c2c5e8b034611b
DeuklyoungKo <deutschgon@gmail.com>
2019-06-11 01:34:54 +0200

    chapter 31 (https://symfonycasts.com/screencast/reactjs)

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import RepLogs from './RepLogs';
 import PropTypes from 'prop-types';
 import uuid from 'uuid';
-import { getRepLogs, deleteRepLog } from "../api/rep_log_api";
+import { getRepLogs, deleteRepLog, createRepLog } from "../api/rep_log_api";
 
 export default class RepLogApp extends Component {
 
@@ -40,20 +40,29 @@ export default class RepLogApp extends Component {
         this.setState({highlightedRowId: repLogId})
     }
 
-    handleAddRepLog(itemLabel, reps) {
+    handleAddRepLog(item, reps) {
         const repLogs = this.state.repLogs;
         const newRep = {
-            id: uuid(),
+            // id: uuid(),
             reps: reps,
-            itemLabel: itemLabel,
-            totalWeightLifted: Math.floor(Math.random()*50)
+            item: item,
+            // totalWeightLifted: Math.floor(Math.random()*50)
         }
 
-        this.setState(prevState => {
-            const newRepLogs = [...prevState.repLogs, newRep];
+        createRepLog(newRep)
+            .then(repLog => {
+                this.setState(prevState => {
+                    const newRepLogs = [...prevState.repLogs, repLog];
 
-            return {repLogs: newRepLogs};
-        })
+                    return {repLogs: newRepLogs};
+                })
+            })
+
+        // this.setState(prevState => {
+        //     const newRepLogs = [...prevState.repLogs, newRep];
+        //
+        //     return {repLogs: newRepLogs};
+        // })
 
     }
 
@@ -73,8 +82,9 @@ export default class RepLogApp extends Component {
                 repLogs: prevState.repLogs.filter(repLog => repLog.id !== id)
             }
         })
-
     }
+
+
 
     render() {
 

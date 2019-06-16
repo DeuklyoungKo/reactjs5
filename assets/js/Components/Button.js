796833bd018778792dab9db4f370fcc4b4092c18
DeuklyoungKo <deutschgon@gmail.com>
2019-06-16 13:17:48 +0200

    chapter 41 (https://symfonycasts.com/screencast/reactjs)

---
+++ assets/js/Components/Button.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default function Button(props) {
+    const { className, ...otherProps } = props;
+
+    console.log(props);
+    console.log(className);
+    console.log({...otherProps});
+
+    return (
+        <button
+            className={`btn ${props.className}`}
+            {...otherProps}
+        >{props.children}</button>
+
+    )
+}
+
+Button.protoTypes = {
+    className: PropTypes.string,
+};
+
+Button.defaultProps = {
+    className: ''
+};
